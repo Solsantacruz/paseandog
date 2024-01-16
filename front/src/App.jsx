6e4a769e03c104dacebd4bd5d73f6a14671ece44
Cristian <cristian84molina@gmail.com>
2024-01-16 19:08:08 -0300

    home provisorio

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import {Routes,Route} from "react-router-dom"
 
 import Login from './views/Login'
+import Home from './views/Home'
+
+
 function App() {
   
 
   return (
     <Routes>
       <Route exact path='/' element={<Login />}/>
+      <Route path='/home' element={<Home />}/>
       </Routes>
   )
 }
